8928b13498cb10f40058abce75b3ac829f92a1dc
MaxBrych <max.brych03@gmail.com>
2023-06-04 18:17:17 +0200

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+    domains: ["cdn.sanity.io", "https://"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
