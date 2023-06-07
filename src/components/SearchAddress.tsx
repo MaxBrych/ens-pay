9fc9d38061e863d5cb47da7bbe8b8d6fdc7d8171
MaxBrych <max.brych03@gmail.com>
2023-06-07 07:38:23 +0200

    returned to old navbar

--- src/components/SearchAddress.tsx
+++ src/components/SearchAddress.tsx
@@ -1,5 +1,3 @@
-// SearchAddress.tsx
-
 import { useState, useRef, FormEvent, useEffect } from "react";
 import { useRouter } from "next/router";
 import {
@@ -10,79 +8,112 @@ import {
   Button,
   useToast,
 } from "@chakra-ui/react";
-import { ethers } from "ethers";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+
+const ethersDynamic: Promise<any> = import("ethers");
 
 const SearchAddress = () => {
-  const [provider, setProvider] = useState<ethers.providers.Provider | null>(
-    null
-  );
-  const [address, setAddress] = useState("");
-  const toast = useToast();
+  const [provider, setProvider] = useState<any>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const addrRef = useRef<HTMLInputElement>(null);
   const router = useRouter();
-  const ref = useRef<HTMLInputElement>(null);
+  const toast = useToast();
 
   useEffect(() => {
-    import("ethers").then((ethers) => {
+    ethersDynamic.then((ethers) => {
       const provider = new ethers.providers.JsonRpcProvider(
-        process.env.NEXT_PUBLIC_PROVIDER_URL
+        "https://spring-red-dinghy.discover.quiknode.pro/54a9c39cdfc6a8085cfb75f545ce67a66249d4ac/"
       );
       setProvider(provider);
     });
   }, []);
 
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
-
-    // Check if ENS is used
-    let checkedAddress: string;
-    if (address.includes(".eth")) {
-      try {
-        const resolvedAddress = await provider?.resolveName(address);
-        if (!resolvedAddress) {
-          throw new Error("ENS name not resolved");
-        } else {
-          checkedAddress = resolvedAddress;
-        }
-      } catch (error: any) {
-        toast({
-          title: "Error",
-          description: error.message,
-          status: "error",
-          duration: 5000,
-          isClosable: true,
-        });
-        return;
-      }
-    } else {
-      checkedAddress = address;
+  const getName = async (addr: string) => {
+    setIsLoading(true);
+    if (provider) {
+      const resolvedName = await provider.lookupAddress(addr);
+      setIsLoading(false);
+      return resolvedName ?? addr;
     }
 
-    // Check address
-    try {
-      ethers.utils.getAddress(checkedAddress);
-      router.push(`/user/${checkedAddress}`);
-    } catch (error) {
-      toast({
-        title: "Error",
-        description: "Invalid address.",
-        status: "error",
-        duration: 5000,
-        isClosable: true,
-      });
+    setIsLoading(false);
+    return addr;
+  };
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (addrRef.current) {
+      if (addrRef.current.value) {
+        getName(addrRef.current.value).then((ensName) => {
+          router.push(`/profile/${ensName}`);
+        });
+      } else {
+        toast({
+          title: "Address is missing.",
+          description: "Please input an address.",
+          status: "error",
+          duration: 3000,
+          isClosable: true,
+        });
+      }
     }
   };
 
   return (
-    <Box my={8}>
-      <form onSubmit={handleSubmit}>
-        <FormControl id="address">
-          <FormLabel>Address</FormLabel>
-          <Input ref={ref} onChange={(e) => setAddress(e.target.value)} />
+    <Box w={"full"}>
+      <Box p={1} className="credit-card" mx="auto" w="full" maxW={"lg"}>
+        <FormControl
+          id="addr"
+          mt={1}
+          justifyContent={"center"}
+          alignItems={"center"}
+          display={"flex"}
+          flexDirection={"column"}
+        >
+          <FormLabel
+            textAlign={"center"}
+            mb={4}
+            textColor={"gray.700"}
+            fontSize={"4xl"}
+            lineHeight={"1.1"}
+            fontWeight={"black"}
+          >
+            Search for your <br />
+            ETH address
+          </FormLabel>
+          <FormLabel
+            mb={8}
+            textAlign={"center"}
+            fontSize={"lg"}
+            fontWeight={"semibold"}
+            textColor={"gray.500"}
+          >
+            We&apos;ll find your ENS name
+          </FormLabel>
+          <Input
+            borderRadius={"2xl"}
+            h={"20"}
+            fontWeight={"bold"}
+            backgroundColor={"white"}
+            fontSize={"xl"}
+            ref={addrRef}
+            placeholder="ETH Address"
+          ></Input>
+          <Button
+            size={"lg"}
+            w={"full"}
+            borderRadius={"full"}
+            mt={6}
+            fontSize={"lg"}
+            colorScheme="blue"
+            onClick={handleSubmit}
+            isLoading={isLoading}
+            loadingText="Resolving..."
+          >
+            Resolve Name
+          </Button>
         </FormControl>
-        <Button mt={4} colorScheme="teal" type="submit">
-          Search
-        </Button>
-      </form>
+      </Box>
     </Box>
   );
 };
